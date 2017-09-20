e91b6d73029c20eae65b63046faef8a9075a98e4
jsviveroj <jsviveroj@unal.edu.co>
2017-09-20 17:42:46 -0500

    Fully tested in server-rancher node1

--- session_ms/session.py
+++ session_ms/session.py
@@ -106,4 +106,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug = True, port = 3001)
+    app.run(debug = True, host = '0.0.0.0',  port = 3001)
