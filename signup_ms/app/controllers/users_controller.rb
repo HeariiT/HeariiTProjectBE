f57539c7859599e3e383317f7e15887c7b5faf01
Unknown <juasmartinezbel@unal.edu.co>
2017-09-23 12:43:03 -0500

    /users/ route and controller removed. Documentation changed

--- signup_ms/app/controllers/users_controller.rb
+++ signup_ms/app/controllers/users_controller.rb
@@ -1,20 +1,5 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, only:[:welcum, :showme]
-
-	def welcum
-	    @user= current_user
-    	render json:          
-    	{ data:
-            {
-              message: "Welcum "+current_user.username
-            }
-       	}
-  	end
-  def index
-    @users = User.all
-    render json: @users
-  end
-
+	
   def show
     @user=User.find(params[:id])
     render json: @user
