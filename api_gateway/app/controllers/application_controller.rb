7df506167075ac7f5309245b68edda752d3c13bd
David Guzmán <djguzmanc@unal.edu.co>
2017-09-22 19:11:36 -0500

    Sign_up controller almost done

--- api_gateway/app/controllers/application_controller.rb
+++ api_gateway/app/controllers/application_controller.rb
@@ -1,25 +1,19 @@
 class ApplicationController < ActionController::API
 
   @@sign_up_ms_url = 'http://192.168.99.101:3000'
+  @@sessions_ms_url = 'http://192.168.99.101:3001'
 
-  def user_id_exists?( user_id )
-    if user_id.nil?
-      msg = {
-        :message => 'User id not provided',
-        :code => 400,
-        :description => 'The user id couldnt be found in the request body'
-      }
-      render :json => msg, :status => :bad_request
-    end
-
-    _response = HTTParty.get( @@sign_up_ms_url + "/users/#{user_id}" )
-    unless _response.include? 'id'
-      msg = {
-        :message => 'Not found',
-        :code => 404,
-        :description => "User with id #{user_id} do not exist"
-      }
-      render :json => msg, :status => :not_found
-    end
+  def jsonify( httparty_results )
+    JSON.parse( httparty_results.body )
   end
+
+  def parse_options( params )
+    return {
+      :body => params.to_json,
+      :headers => {
+        'Content-Type' => 'application/json'
+      }
+    }
+  end
+
 end
--- session_ms/session.py
+++ session_ms/session.py
@@ -103,7 +103,7 @@ def logout():
 
 if __name__ == "__main__":
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+    #    db.create_all()
 
     app.run(debug = True, host = '0.0.0.0',  port = 3001)
